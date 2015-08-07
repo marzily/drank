976c664709ed26354f32e87fa450c32aa56a8331
margie <ly.margarett@gmail.com>
2015-08-07 16:26:32 -0600

    commiting before switching branches

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'landing_pages#index'
 
   # get 'auth/github', as: "login"
-    # get 'oauth/callback', to: 'sessions#create'
+  get 'oauth/callback', to: 'sessions#create'
     # @callback_url = "http://127.0.0.1:3000/oauth/callback"
 end
