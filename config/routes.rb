b62e42c12b0a1012adbbb66de3403d51059c83e0
margie <ly.margarett@gmail.com>
2015-08-08 10:37:22 -0600

    configure new landing page, setting up base functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'landing_pages#index'
+  root 'landing_pages#show'
+  resources :landing_pages, only: [:index]
 
   # get 'auth/github', as: "login"
   get 'oauth/callback', to: 'sessions#create'
