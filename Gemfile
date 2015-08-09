c291941fa06023cf7de6a24feff08884e21e8aed
margie <ly.margarett@gmail.com>
2015-08-08 22:56:03 -0600

    add capybara gem to Gemfile

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   gem 'web-console', '~> 2.0'
   gem 'spring'
   gem 'rspec-rails'
+  gem 'capybara'
 end
 
 group :production do
--- spec/views/static_pages/landing_page.html.erb_spec.rb
+++ spec/views/static_pages/landing_page.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "static_pages/landing_page.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    assign(:current_user, nil)
+  end
+
+  it "has the name of the app" do
+    render
+    # expect(response).to have_http_status(:success)
+    rendered.should contain("Drank")
+  end
 end
