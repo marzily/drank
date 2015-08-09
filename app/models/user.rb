70f3a5358eefeec5b67c9664c9136e7fd0438b19
margie <ly.margarett@gmail.com>
2015-08-08 22:16:16 -0600

    add image url column to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ActiveRecord::Base
     if user = find_by(uid: auth_info.extra.raw_info.user_id)
       user
     else
-      create({name: auth_info.extra.raw_info.name,
-              screen_name: auth_info.extra.raw_info.screen_name,
-              uid: auth_info.extra.raw_info.user_id,
+      create({name: auth_info.info.name,
+              screen_name: auth_info.info.nickname,
+              uid: auth_info.uid,
+              image: auth_info.info.image,
               oauth_token: auth_info.credentials.token,
               oauth_token_secret: auth_info.credentials.secret
               })
