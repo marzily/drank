58e50b2618cc22c66a6a218a151e9c7f7dbbf085
margie <ly.margarett@gmail.com>
2015-08-11 11:40:57 -0600

    create drinks and weathers in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -7,9 +8,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # weather ranges
-  cold = Weather.create(min_temp: nil, max_temp: 69)
-  mild = Weather.create(min_temp: 70, max_temp: 89)
-  hot = Weather.create(min_temp: 90, max_temp: nil)
+cold = Weather.create(min_temp: nil, max_temp: 69)
+mild = Weather.create(min_temp: 70, max_temp: 89)
+hot = Weather.create(min_temp: 90, max_temp: nil)
 
 # drinks
 hot_drinks = [ "hot chocolate", "coffee", "tea", "latte", "mocha", "espresso",
@@ -20,6 +21,16 @@ iced_drinks = [ "iced tea", "juice", "iced coffee", "iced espresso drinks",
 
 ice_blended_drinks = [ "smoothies", "ice blended drinks", "slushies" ]
 
-hot_drinks.each do |hot_drink|
-  cold.drinks.create(drink_type: hot_drink)
+def create_drinks(drink_types, weather)
+  drink_types.map do |drink_type|
+    weather.drinks.create(drink_type: drink_type)
+  end
+end
+
+hot_drink_objects         = create_drinks(hot_drinks, cold)
+iced_drink_objects        = create_drinks(iced_drinks, mild)
+ice_blended_drink_objects = create_drinks(ice_blended_drinks, hot)
+
+iced_drink_objects.each do |drink|
+  WeatherDrink.create(drink_id: drink.id, weather_id: hot.id)
 end
