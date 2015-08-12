b1708fd408fae497a146d2f82af1529346a67aba
margie <ly.margarett@gmail.com>
2015-08-12 14:36:47 -0600

    display location pin on map

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-# Rails.application.config.assets.precompile += %w( landing_pages.js map.js)
+Rails.application.config.assets.precompile += %w( landing_page.js map.js)
