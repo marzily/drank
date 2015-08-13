d3f336f677e3df06c99969dbb0f5071662391e0b
margie <ly.margarett@gmail.com>
2015-08-13 09:29:09 -0600

    remove unnecessary files and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#landing_page'
-  # resources :landing_pages, only: [:index]#, param: :location
 
   get 'auth/twitter', as: "login"
   get 'auth/twitter/callback', to: 'sessions#create'
