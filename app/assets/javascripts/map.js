e95fa67e8a1034bf3b6f0a1211698f82573d3932
margie <ly.margarett@gmail.com>
2015-08-12 18:04:29 -0600

    successfully display google javascript map(!!!), add sass rails gem, import map.css.sass into application.css.sass

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -11,3 +11,28 @@
 // $(document).ready(function() {
 //   console.log("hello from map.js");
 // });
+
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.7514438, lng: -105.0718248},
+    zoom: 14
+  });
+}
+
+var mapURL = "<script async defer
+  src='https://maps.googleapis.com/maps/api/js?key=AIzaSyBD1ckF76MqtDJReVqr84PzSrR2X8gJRL8&callback='" + initMap + ">
+  </script>"
+
+function appendMapURL() {
+  $( "#map" ).append( mapURL );
+  // var container = document.getElementById("js");
+  // container.innerHTML = mapURL;
+  console.log( "display map!" );
+}
+
+
+$(document).ready(function() {
+  appendMapURL();
+});
--- app/assets/javascripts/user_show.js
+++ app/assets/javascripts/user_show.js
@@ -1,12 +1,48 @@
 function dropdownItemSelect() {
-  $('.dropdown-menu').children().children().on("click", function() {
-    $('.recommended').text( $(this).text() );
-    console.log( $( this ).text() );
+  $('.dropdown-menu').children().children().on("click", updateRecommended);
+}
+
+function updateRecommended() {
+  $('.recommended').text( $(this).text() );
+  console.log( $( this ).text() );
+}
+
+// on click
+//  ==> drop image
+// ==> get new image url
+// ==> append new image
+
+function updateMap(position) {
+  var coordinates = { latitude: position.coords.latitude,
+                      longitude: position.coords.longitude };
+  $.ajax({  url: "/map_coords",
+            type: "POST",
+            data: coordinates,
+            success: function() {
+                console.log(coordinates);
+            },
+            error: function(message){
+                console.error(message);
+            }
+        });
+}
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.7514438, lng: -105.0718248},
+    zoom: 14
   });
 }
 
+var map_url = "<script async defer src='https://maps.googleapis.com/maps/api/js?key=AIzaSyBD1ckF76MqtDJReVqr84PzSrR2X8gJRL8&callback=initMap'></script>"
 
+function appendMapURL() {
+  $( "html" ).append( map_url );
+  console.log( "display map!" );
+}
 
+appendMapURL();
 $(document).ready(function() {
   dropdownItemSelect();
 });
