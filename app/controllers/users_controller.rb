0fca25f45c6f125a48f187cccb5ec9941ac67540
margie <ly.margarett@gmail.com>
2015-08-23 21:46:55 -0600

    update seed file, move drink selections by weather to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,17 @@ class UsersController < ApplicationController
 
   def show
     # @restaurants = restaurants
+    @drinks = drinks_by_temp
+    session[:drink_type] = @drinks.sample.drink_type
     # byebug
   end
 
   def reroute_unauth_user
     redirect_to root_path unless signed_in?
+  end
+
+  def drinks_by_temp
+    Weather.current_range(session[:temp_f]).drinks
   end
 
   def restaurants
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,16 +3,6 @@ module UsersHelper
     session[:location]
   end
 
-  def drinks_by_temp
-    # @drinks ||= Weather.current_range(current_conditions).drinks
-  end
-
-  def drink_recommendation
-    return session[:drink_type] unless session[:drink_type].nil?
-    drink = drinks_by_temp.sample
-    session[:drink_type] = drink.drink_type
-  end
-
   def search_client
     @search_client ||= SearchAPI.new
   end
