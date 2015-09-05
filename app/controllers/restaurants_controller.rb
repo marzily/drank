b9fbc1e2b47f35761551c96947df7d7ccc5773e7
margie <ly.margarett@gmail.com>
2015-09-05 11:09:35 -0600

    update vars in users_controller for view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,8 @@
 class RestaurantsController < ApplicationController
   def drink
-    session[:drink] = params[:drink]
-    restaurants = search_client.businesses(session[:drink], session[:location]['latitude'], session[:location]['longitude'])
+    @restaurants = search_client.businesses(session[:drink], session[:location]['latitude'], session[:location]['longitude'])
 
-    render partial: "users/restaurants", locals: { restaurants: restaurants }
+    render partial: "users/restaurants"
   end
 
   def search_client
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   before_action :reroute_unauth_user
 
   def show
-    @restaurants = "hello"
     @drinks = Weather.current_range(session[:temp_f]).drinks
+    @recommended = @drinks.sample.drink_type
+    @restaurants = SearchAPI.new.businesses(@recommended, session[:location]['latitude'], session[:location]['longitude'])
   end
 
   def reroute_unauth_user
