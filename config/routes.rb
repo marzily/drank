74b423cd6d209acfcebae45ccdb6658bd4473b1d
margie <ly.margarett@gmail.com>
2015-11-12 22:15:00 -0800

    rm ajax call sending city/state to server

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
   resource :users, only: [:show]
 
   post '/coords', to: 'locations#coords'
-  get '/city_state', to: 'locations#city_state'
-
-  get '/current_conditions', to: 'weather#current_conditions'
-
-  get '/drink', to: 'restaurants#drink'
+  get  '/current_conditions', to: 'weather#current_conditions'
+  get  '/drink', to: 'restaurants#drink'
 end
