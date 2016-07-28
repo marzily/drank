57085de60fa7a63b8f334261d66869715e16946e
margie <ly.margarett@gmail.com>
2016-07-28 16:39:06 -0700

    show params temp in show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :reroute_unauth_user
 
   def show
+    @temp = "this is the params temp: #{params['temp_f']}"
     @drinks = Weather.current_range(params["temp_f"]).drinks
     @recommended = @drinks.sample.drink_type
   end
