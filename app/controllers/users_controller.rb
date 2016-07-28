c58abf22992eb5b614a8ff741d0a2c67446d0d41
margie <ly.margarett@gmail.com>
2016-07-28 16:26:17 -0700

    show weather current range max

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @temp = params[:temp_f]
-    @range = Weather.current_range(params[:temp_f])
+    @range = Weather.current_range(params[:temp_f]).max_temp
     @drinks = Weather.current_range(params[:temp_f]).drinks
     @recommended = @drinks.sample.drink_type
   end
